4a3fa09b27b79610be2579030f17b0647aef2388
Nsikak Emmanuel Eyoma <35346300+neeyoma@users.noreply.github.com>
2025-07-18 17:32:03 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,95 @@
+# Configuring the Azure Provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.1"
+    }
+  }
+}
+
+# Configuring the Microsoft Azure Provider
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
+
+# To get current client configuration
+data "azurerm_client_config" "current" {}
+
+# Creating Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = var.tags
+}
+
+# Generating random password for the SQL admin user
+resource "random_password" "sql_admin_password" {
+  length  = 16
+  special = true
+}
+
+# Deploying the Key Vault module
+module "key_vault" {
+  source = "./modules/key_vault"
+
+  resource_group_name = azurerm_resource_group.main.name
+  location           = azurerm_resource_group.main.location
+  tenant_id          = data.azurerm_client_config.current.tenant_id
+  object_id          = data.azurerm_client_config.current.object_id
+  sql_admin_password = random_password.sql_admin_password.result
+  tags               = var.tags
+}
+
+# Deploying the networking module
+module "networking" {
+  source = "./modules/networking"
+
+  resource_group_name = azurerm_resource_group.main.name
+  location           = azurerm_resource_group.main.location
+  vnet_name          = var.vnet_name
+  vnet_address_space = var.vnet_address_space
+  web_subnet_prefix  = var.web_subnet_prefix
+  app_subnet_prefix  = var.app_subnet_prefix
+  db_subnet_prefix   = var.db_subnet_prefix
+  tags               = var.tags
+}
+
+# Deploying the compute module
+module "compute" {
+  source = "./modules/compute"
+
+  resource_group_name               = azurerm_resource_group.main.name
+  location                         = azurerm_resource_group.main.location
+  web_subnet_id                    = module.networking.web_subnet_id
+  app_subnet_id                    = module.networking.app_subnet_id
+  db_subnet_id                     = module.networking.db_subnet_id
+  vnet_id                          = module.networking.vnet_id
+  load_balancer_backend_pool_id    = module.networking.load_balancer_backend_pool_id
+  admin_username                   = var.admin_username
+  sql_admin_password               = random_password.sql_admin_password.result
+  key_vault_id                     = module.key_vault.key_vault_id
+  tags                             = var.tags
+}
+
+# Deploying the security module
+module "security" {
+  source = "./modules/security"
+
+  resource_group_name = azurerm_resource_group.main.name
+  location           = azurerm_resource_group.main.location
+  web_subnet_id      = module.networking.web_subnet_id
+  app_subnet_id      = module.networking.app_subnet_id
+  db_subnet_id       = module.networking.db_subnet_id
+  tags               = var.tags
+}
