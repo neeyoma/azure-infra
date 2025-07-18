8cc126f65cfb25ba8d04b8be3b550736cad518b0
Nsikak Emmanuel Eyoma <35346300+neeyoma@users.noreply.github.com>
2025-07-18 18:36:59 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,30 +25,23 @@ provider "azurerm" {
 # To get current client configuration
 data "azurerm_client_config" "current" {}
 
+# Existing Key Vault and secret
+data "azurerm_key_vault" "existing" {
+  name                = var.existing_key_vault_name
+  resource_group_name = var.existing_key_vault_rg
+}
+
+data "azurerm_key_vault_secret" "sql_admin_password" {
+  name         = var.secret_name
+  key_vault_id = data.azurerm_key_vault.existing.id
+}
+
 # Creating Resource Group
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
 
   tags = var.tags
-}
-
-# Generating random password for the SQL admin user
-resource "random_password" "sql_admin_password" {
-  length  = 16
-  special = true
-}
-
-# Deploying the Key Vault module
-module "key_vault" {
-  source = "./modules/key_vault"
-
-  resource_group_name = azurerm_resource_group.main.name
-  location           = azurerm_resource_group.main.location
-  tenant_id          = data.azurerm_client_config.current.tenant_id
-  object_id          = data.azurerm_client_config.current.object_id
-  sql_admin_password = random_password.sql_admin_password.result
-  tags               = var.tags
 }
 
 # Deploying the networking module
@@ -77,7 +70,7 @@ module "compute" {
   vnet_id                          = module.networking.vnet_id
   load_balancer_backend_pool_id    = module.networking.load_balancer_backend_pool_id
   admin_username                   = var.admin_username
-  sql_admin_password               = random_password.sql_admin_password.result
+  sql_admin_password               = data.azurerm_key_vault_secret.sql_admin_password.value
   key_vault_id                     = module.key_vault.key_vault_id
   tags                             = var.tags
 }
